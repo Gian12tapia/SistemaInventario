3f170f745579dadff64dce22c3070444c3c97632
Gian12tapia <12nano2000@gmail.com>
2023-11-27 16:57:12 -0500

    CRUD de Bodegas de la Farmacia

--- SistemaInventario.Utilidades/obj/Debug/net6.0/SistemaInventario.Utilidades.AssemblyInfo.cs
+++ SistemaInventario.Utilidades/obj/Debug/net6.0/SistemaInventario.Utilidades.AssemblyInfo.cs
@@ -15,7 +15,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("SistemaInventario.Utilidades")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+4bf4c2ca176a18b82f308cec9063267002153f54")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+a3cc2c7da635836a31597e906a631db8ef04a884")]
 [assembly: System.Reflection.AssemblyProductAttribute("SistemaInventario.Utilidades")]
 [assembly: System.Reflection.AssemblyTitleAttribute("SistemaInventario.Utilidades")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
